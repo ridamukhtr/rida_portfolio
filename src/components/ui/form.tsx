e9d7129ef41e4c7133a79c17a28988d4d7179a45
ridamukhtr <ridamukhtar638@gmail.com>
2026-03-25 19:25:27 +0500

    paths set

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { Slot } from "@radix-ui/react-slot";
-import { Controller, ControllerProps, FieldPath, FieldValues, FormProvider, useFormContext } from "react-hook-form";
+import { Controller, FormProvider, useFormContext, type ControllerProps, type FieldPath, type FieldValues } from "react-hook-form";
 
 import { cn } from "../../lib/utils";
-import { Label } from "@/components/ui/label";
+import { Label } from "../../components/ui/label";
 
 const Form = FormProvider;
 
